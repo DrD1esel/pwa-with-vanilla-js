61d0f3bf7fc92f03b75164a82c6d4d57a041e4d7
Yauhen <kolonicki.e@gmail.com>
2022-08-09 18:27:03 +0300

    ad

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,35 +1,34 @@
-const staticDevCoffee = "dev-coffee-site-v5";
-const cacheName = "dev-coffee-site-v1";
+const staticDevCoffee = 'dev-coffee-site-v6';
+const cacheName = 'dev-coffee-site-v1';
 const assets = [
-  "/",
-  "/index.html",
-  "/css/style.css",
-  "/js/app.js",
-  "/images/coffee1.jpg",
-  "/images/coffee2.jpg",
-  "/images/coffee3.jpg",
-  "/images/coffee4.jpg",
-  "/images/coffee5.jpg",
-  "/images/coffee6.jpg",
-  "/images/coffee7.jpg",
-  "/images/coffee8.jpg",
-  "/images/coffee9.jpg",
+  '/',
+  '/index.html',
+  '/css/style.css',
+  '/js/app.js',
+  '/images/coffee1.jpg',
+  '/images/coffee2.jpg',
+  '/images/coffee3.jpg',
+  '/images/coffee4.jpg',
+  '/images/coffee5.jpg',
+  '/images/coffee6.jpg',
+  '/images/coffee7.jpg',
+  '/images/coffee8.jpg',
+  '/images/coffee9.jpg',
 ];
 
-self.addEventListener("install", (installEvent) => {
+self.addEventListener('install', installEvent => {
   installEvent.waitUntil(caches.delete(cacheName));
   installEvent.waitUntil(
-    caches.open(cacheName).then((cache) => {
+    caches.open(cacheName).then(cache => {
       cache.addAll(assets);
     })
   );
-  window.location.reload();
-  console.log("Install");
+  console.log('Install');
 });
 
-self.addEventListener("fetch", (fetchEvent) => {
+self.addEventListener('fetch', fetchEvent => {
   fetchEvent.respondWith(
-    caches.match(fetchEvent.request).then((res) => {
+    caches.match(fetchEvent.request).then(res => {
       return res || fetch(fetchEvent.request);
     })
   );
