94ec4d2730573946089b44da1af3e4604b2867b1
Yauhen Kalanitski <ykalanitski@ibagroup.eu>
2022-08-10 09:16:24 +0300

    a

--- js/app.js
+++ js/app.js
@@ -1,44 +1,44 @@
-const container = document.querySelector('.container');
+const container = document.querySelector(".container");
 const coffees = [
   {
-    name: 'Perspiciatis',
-    image: 'images/coffee1.jpg',
+    name: "Perspiciatis",
+    image: "images/coffee1.jpg",
   },
   {
-    name: 'Voluptatem',
-    image: 'images/coffee2.jpg',
+    name: "Voluptatem",
+    image: "images/coffee2.jpg",
   },
   {
-    name: 'Explicabo',
-    image: 'images/coffee3.jpg',
+    name: "Explicabo",
+    image: "images/coffee3.jpg",
   },
   {
-    name: 'Rchitecto',
-    image: 'images/coffee4.jpg',
+    name: "Rchitecto",
+    image: "images/coffee4.jpg",
   },
   {
-    name: ' Beatae',
-    image: 'images/coffee5.jpg',
+    name: " Beatae",
+    image: "images/coffee5.jpg",
   },
   {
-    name: ' Vitae',
-    image: 'images/coffee6.jpg',
+    name: " Vitae",
+    image: "images/coffee6.jpg",
   },
   {
-    name: 'Inventore',
-    image: 'images/coffee7.jpg',
+    name: "Inventore",
+    image: "images/coffee7.jpg",
   },
   {
-    name: 'Veritatis',
-    image: 'images/coffee8.jpg',
+    name: "Veritatis",
+    image: "images/coffee8.jpg",
   },
   {
-    name: 'Accusantium',
-    image: 'images/coffee9.jpg',
+    name: "Accusantium",
+    image: "images/coffee9.jpg",
   },
 ];
 const showCoffees = () => {
-  let output = '';
+  let output = "";
   coffees.forEach(
     ({ name, image }) =>
       (output += `
@@ -52,71 +52,68 @@ const showCoffees = () => {
   container.innerHTML = output;
 };
 
-document.addEventListener('DOMContentLoaded', showCoffees);
+document.addEventListener("DOMContentLoaded", showCoffees);
 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function () {
-    // navigator.serviceWorker
-    //   .register('/serviceWorker.js')
-    //   .then(res => {
-    //     console.log(res);
-    //     console.log('service worker registered');
-    //   })
-    //   .catch(err => console.log('service worker not registered', err));
-    let updated = false;
-    let activated = false;
-    navigator.serviceWorker.register('/serviceWorker.js').then(regitration => {
-      regitration.addEventListener('updatefound', () => {
-        const worker = regitration.installing;
-        worker.addEventListener('statechange', () => {
-          console.log({ state: worker.state });
-          if (worker.state === 'activated') {
-            // Here is when the activated state was triggered from the lifecycle of the service worker.
-            // This will trigger on the first install and any updates.
-            activated = true;
-            checkUpdate();
-          }
-        });
-      });
-    });
-    navigator.serviceWorker.addEventListener('controllerchange', () => {
-      // This will be triggered when the service worker is replaced with a new one.
-      // We do not just reload the page right away, we want to make sure we are fully activated using the checkUpdate function.
-      console.log({ state: 'updated' });
-      updated = true;
-      checkUpdate();
-    });
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function () {
+    navigator.serviceWorker
+      .register("/serviceWorker.js")
+      .then((res) => console.log("service worker registered"))
+      .catch((err) => console.log("service worker not registered", err));
+    // let updated = false;
+    // let activated = false;
+    // navigator.serviceWorker.register('/serviceWorker.js').then(regitration => {
+    //   regitration.addEventListener('updatefound', () => {
+    //     const worker = regitration.installing;
+    //     worker.addEventListener('statechange', () => {
+    //       console.log({ state: worker.state });
+    //       if (worker.state === 'activated') {
+    //         // Here is when the activated state was triggered from the lifecycle of the service worker.
+    //         // This will trigger on the first install and any updates.
+    //         activated = true;
+    //         checkUpdate();
+    //       }
+    //     });
+    //   });
+    // });
+    // navigator.serviceWorker.addEventListener('controllerchange', () => {
+    //   // This will be triggered when the service worker is replaced with a new one.
+    //   // We do not just reload the page right away, we want to make sure we are fully activated using the checkUpdate function.
+    //   console.log({ state: 'updated' });
+    //   updated = true;
+    //   checkUpdate();
+    // });
 
-    function checkUpdate() {
-      if (activated && updated) {
-        console.log('Application was updated refreshing the page...');
-        window.location.reload();
-      }
-    }
+    // function checkUpdate() {
+    //   if (activated && updated) {
+    //     console.log('Application was updated refreshing the page...');
+    //     window.location.reload();
+    //   }
+    // }
   });
 }
 
-Notification.requestPermission(result => {
-  if (result === 'granted') {
+Notification.requestPermission((result) => {
+  if (result === "granted") {
   }
 });
 
-const sendButton = document.getElementById('send-button');
-const sendButton2 = document.getElementById('send-button2');
+const sendButton = document.getElementById("send-button");
+const sendButton2 = document.getElementById("send-button2");
 
-sendButton.addEventListener('click', () => {
-  console.log('click');
-  if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.getRegistration().then(sw => sw.showNotification('Hello'), {
+sendButton.addEventListener("click", () => {
+  console.log("click");
+  if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.getRegistration().then((sw) => sw.showNotification("Hello"), {
       vibrate: [200, 100, 200, 100, 200, 100, 200],
     });
   }
 });
 
-sendButton2.addEventListener('click', () => {
+sendButton2.addEventListener("click", () => {
   setTimeout(() => {
-    if ('serviceWorker' in navigator) {
-      navigator.serviceWorker.getRegistration().then(sw => sw.showNotification('Hello'), {
+    if ("serviceWorker" in navigator) {
+      navigator.serviceWorker.getRegistration().then((sw) => sw.showNotification("Hello"), {
         vibrate: [200, 100, 200, 100, 200, 100, 200],
       });
     }
--- serviceWorker.js
+++ serviceWorker.js
@@ -1,34 +1,50 @@
-const staticDevCoffee = 'dev-coffee-site-v9';
-const cacheName = 'dev-coffee-site-v1';
+const staticDevCoffee = "dev-coffee-site-v9";
+const cacheName = "v3";
 const assets = [
-  '/',
-  '/index.html',
-  '/css/style.css',
-  '/js/app.js',
-  '/images/coffee1.jpg',
-  '/images/coffee2.jpg',
-  '/images/coffee3.jpg',
-  '/images/coffee4.jpg',
-  '/images/coffee5.jpg',
-  '/images/coffee6.jpg',
-  '/images/coffee7.jpg',
-  '/images/coffee8.jpg',
-  '/images/coffee9.jpg',
+  "/",
+  "/index.html",
+  "/css/style.css",
+  "/js/app.js",
+  "/images/coffee1.jpg",
+  "/images/coffee2.jpg",
+  "/images/coffee3.jpg",
+  "/images/coffee4.jpg",
+  "/images/coffee5.jpg",
+  "/images/coffee6.jpg",
+  "/images/coffee7.jpg",
+  "/images/coffee8.jpg",
+  "/images/coffee9.jpg",
 ];
 
-self.addEventListener('install', installEvent => {
+self.addEventListener("activate", (event) => {
+  const cacheKeeplist = [cacheName];
+
+  event.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (cacheKeeplist.indexOf(key) === -1) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener("install", (installEvent) => {
   installEvent.waitUntil(caches.delete(cacheName));
   installEvent.waitUntil(
-    caches.open(cacheName).then(cache => {
+    caches.open(cacheName).then((cache) => {
       cache.addAll(assets);
     })
   );
-  console.log('Install');
+  console.log("Install");
 });
 
-self.addEventListener('fetch', fetchEvent => {
+self.addEventListener("fetch", (fetchEvent) => {
   fetchEvent.respondWith(
-    caches.match(fetchEvent.request).then(res => {
+    caches.match(fetchEvent.request).then((res) => {
       return res || fetch(fetchEvent.request);
     })
   );
